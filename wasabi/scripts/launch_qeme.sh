2f70a2a79bc1c5767111d13e01aa43dee3dd1d6a
Masaru Fukazawa <upw.ponsuke@gmail.com>
2025-10-13 20:34:17 +0900

    p68:ビルドや実行を簡単にする。buildはDocker,実行はローカルPCなので実行はlaunch_qeme.shを直接叩く感じ

---
+++ wasabi/scripts/launch_qeme.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+PROJ_ROOT="$(dirname $(dirname ${BASH_SOURCE:-$0}))"
+cd "${PROJ_ROOT}"
+
+PATH_TO_EFI="target/x86_64-unknown-uefi/debug/wasabi.efi"
+
+rm -rf mnt
+mkdir -p mnt/EFI/BOOT
+cp ${PATH_TO_EFI} mnt/EFI/BOOT/BOOTX64.EFI
+
+qemu-system-x86_64 \
+    -bios 3rd_party/ovmf/RELEASEX64_OVMF.fd \
+    -drive format=raw,file=fat:rw:mnt \
+    -boot menu=off
